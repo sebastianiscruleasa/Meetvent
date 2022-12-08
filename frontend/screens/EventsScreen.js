16be17e4175e2cd5a994dd952f331358f96b0d73
Iscruleasa Sebastian <sebi.iscruleasa@yahoo.ro>
2022-12-08 09:39:58 +0200

    added dummy pages for implementing the navigation

---
+++ frontend/screens/EventsScreen.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text} from "react-native";
+
+function EventsScreen() {
+    return (
+        <Text>EventsScreen</Text>
+    )
+}
+
+export default EventsScreen;
+
+const styles = StyleSheet.create({
+
+})
---
+++ frontend/screens/ProfileScreen.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text} from "react-native";
+
+function ProfileScreen() {
+    return (
+        <Text>ProfileScreen</Text>
+    )
+}
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+
+})
