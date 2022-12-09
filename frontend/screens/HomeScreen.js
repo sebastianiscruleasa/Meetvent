d4264aad1853a23a2e7e6f1f462b2c9df6e86aeb
Iscruleasa Sebastian <sebi.iscruleasa@yahoo.ro>
2022-12-09 14:27:50 +0200

    added event preview card

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,13 +1,75 @@
-import {StyleSheet, Text} from "react-native";
+import {FlatList, StyleSheet} from "react-native";
+import EventsPreviewCard from "../components/Events/EventsPreviewCard";
+
+const DUMMY_EVENTS = [
+    {
+        id: 1,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 2,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 3,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 4,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 5,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 6,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 7,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+    {
+        id: 8,
+        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
+        date: {day: 10, month: "JUNE"},
+        title: "International Gala Music Festival",
+        location: "36 Guild Street London, UK"
+    },
+]
 
 function HomeScreen() {
     return (
-        <Text>HomeScreen</Text>
+        <FlatList horizontal={true} data={DUMMY_EVENTS} keyExtractor={(event) => event.id} renderItem={(itemData) =>
+            <EventsPreviewCard id={itemData.item.id} image={itemData.item.image} title={itemData.item.title}
+                               date={itemData.item.date.day}
+                               location={itemData.item.location}/>
+        }/>
     )
 }
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({})
