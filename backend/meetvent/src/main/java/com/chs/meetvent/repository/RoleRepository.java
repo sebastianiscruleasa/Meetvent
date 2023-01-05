bd763f2da6731d44ea1032ee9f94058e890ba7e4
mjurj <mihai.jurj@irian.ro>
2023-01-05 19:31:37 +0200

    Remove Role Class

--- backend/meetvent/src/main/java/com/chs/meetvent/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chs.meetvent.repository;
-
-import com.chs.meetvent.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- backend/meetvent/src/main/java/com/chs/meetvent/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chs.meetvent.repository;
-import com.chs.meetvent.domain.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findAppUserByUsername(String username);
-}
