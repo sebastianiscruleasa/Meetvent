8bcf853999203fc9551bc195b2f05f17fab698bf
Mihai405 <mihaijurj2000@yahoo.com>
2023-01-10 20:17:47 +0200

    Add JsonMessageResponse + Delete TestObj request

--- backend/meetvent/src/main/java/com/chs/meetvent/controllers/AuthController.java
+++ backend/meetvent/src/main/java/com/chs/meetvent/controllers/AuthController.java
@@ -2,10 +2,9 @@ package com.chs.meetvent.controllers;
 
 import com.chs.meetvent.domain.AppUser;
 import com.chs.meetvent.domain.dto.AuthTokenDTO;
-import com.chs.meetvent.domain.dto.TestDTO;
+import com.chs.meetvent.domain.dto.JSONMessageResponse;
 import com.chs.meetvent.jwt.JwtUtils;
 import com.chs.meetvent.repository.AppUserRepository;
-import com.chs.meetvent.service.UserDetailsImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,11 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
@@ -74,25 +68,20 @@ public class AuthController {
         if (appUserRepository.existsAppUserByUsername(appUser.getUsername())) {
             return ResponseEntity
                     .badRequest()
-                    .body("Error: Username is already taken!");
+                    .body(new JSONMessageResponse("Error: Username is already taken!"));
         }
 
         if (appUserRepository.existsAppUserByEmail(appUser.getEmail())) {
             return ResponseEntity
                     .badRequest()
-                    .body("Error: Email is already in use!");
+                    .body(new JSONMessageResponse("Error: Email is already in use!"));
         }
 
         // Create new user's account
         appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
         appUserRepository.save(appUser);
 
-        return ResponseEntity.ok(new AuthTokenDTO("User registered successfully!"));
-    }
-
-    @GetMapping("/test")
-    public TestDTO testController() {
-        return new TestDTO("test");
+        return ResponseEntity.ok(new JSONMessageResponse("User registered successfully!"));
     }
 
 }
