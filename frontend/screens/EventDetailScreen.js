af4de35bea6062ad22dccdbb06aa44a497831393
Iscruleasa Sebastian <sebi.iscruleasa@yahoo.ro>
2023-01-10 23:28:01 +0200

    implemented the request for getting an event based on the id. Location api token expired and I've changed it

--- frontend/screens/EventDetailScreen.js
+++ frontend/screens/EventDetailScreen.js
@@ -1,144 +1,78 @@
-import {Image, ScrollView, StyleSheet, Text, View} from "react-native";
+import {Alert, Image, ScrollView, StyleSheet, Text, View} from "react-native";
 import EventDetailRow from "../components/Events/EventDetailRow";
 import colors from "../constants/colors";
 import ButtonContainedLarge from "../components/ui/ButtonContainedLarge";
-
-const DUMMY_EVENTS = [
-    {
-        id: 1,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai1",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 2,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai2",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 3,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai3",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 4,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai4",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 5,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai5",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 6,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai6",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 7,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai7",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-    {
-        id: 8,
-        image: "https://media.resources.festicket.com/www/photos/3694-artwork.jpg",
-        date: "14 December 2023",
-        time: "Sun, 12:15 PM - 14:30 PM",
-        title: "International Gala Music Festival",
-        location: "Gala Convention Center",
-        address: "36 Guild Street London, UK",
-        organizer: {
-            name: "Jurj Mihai8",
-            photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-        },
-        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
-    },
-]
+import {AuthContext} from "../store/auth-context";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {useCallback, useContext, useEffect, useState} from "react";
 
 function EventDetailScreen({route}) {
     const eventId = route.params.eventId;
 
-    const event = DUMMY_EVENTS.find((event) => event.id === eventId);
+    const [isLoading, setIsLoading] = useState(false);
+    const [event, setEvent] = useState({});
 
-    const {image, title, date, time, location, address, organizer, description} = event
+    const authCtx = useContext(AuthContext);
+
+    const fetchEvent = useCallback(async () => {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:8080/events/${eventId}`, {
+            headers: {
+                "Authorization": `Bearer ${authCtx.token}`
+            },
+        })
+        if (!response.ok) {
+            Alert.alert(
+                'Something went wrong!',
+                'Please try again later!'
+            );
+        } else {
+            const data = await response.json();
+            const address = `${data.address.street}, ${data.address.city}`;
+            setEvent({...data, "address": address});
+        }
+        setIsLoading(false);
+    }, [eventId])
+
+    useEffect(() => {
+        fetchEvent();
+    }, [fetchEvent])
+
+
+    if (isLoading || !event) {
+        return <LoadingOverlay/>;
+    }
+
+    const image = "https://media.resources.festicket.com/www/photos/3694-artwork.jpg"
+    const organizer = {
+        name: "Jurj Mihai8",
+        photo: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
+    }
+
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    let newDate = new Date(event.date);
+    const updatedDate = newDate.toLocaleDateString("en-US", options)
 
     return (
         <View>
             <Image style={styles.image} source={{uri: image}}/>
-            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.title}>{event.title}</Text>
             <ScrollView style={styles.container}>
-                <EventDetailRow title={date} details={time} icon="calendar"/>
-                <EventDetailRow title={location} details={address} icon="location"/>
+                <EventDetailRow title={updatedDate} details={event.time} icon="calendar"/>
+                <EventDetailRow title={event.location} details={event.address} icon="location"/>
                 <EventDetailRow title={organizer.name} details="Organizer" image={organizer.photo}/>
                 <Text style={styles.about}>About Event</Text>
-                <Text style={styles.description}>{description}</Text>
+                <Text style={styles.description}>{event.description}</Text>
             </ScrollView>
             <View style={styles.buttonContainer}>
                 <ButtonContainedLarge color={colors.primary500} icon="arrow-forward-circle-outline" iconSize={24}
-                                 iconOnTheRight={true}>GOING</ButtonContainedLarge>
+                                      iconOnTheRight={true}>GOING</ButtonContainedLarge>
             </View>
         </View>
     )
