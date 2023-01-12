fdaadf9f6e270f2198caaed37a78bf15c44f5333
Iscruleasa Sebastian <sebi.iscruleasa@yahoo.ro>
2023-01-12 07:20:34 +0200

    working logic for the connection screen and the connection are now added to the chat screen

--- frontend/screens/ChatScreen.js
+++ frontend/screens/ChatScreen.js
@@ -1,9 +1,80 @@
-import {Text} from "react-native";
+import {Alert, FlatList, StyleSheet, Text, View} from "react-native";
+import {useCallback, useContext, useEffect, useState} from "react";
+import {AuthContext} from "../store/auth-context";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import colors from "../constants/colors";
+import ChatCard from "../components/Profile/ChatCard";
 
 function ChatScreen() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [connections, setConnections] = useState(false);
+    const authCtx = useContext(AuthContext);
+
+    const fetchEvent = useCallback(async () => {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:8080/tinder/matches`, {
+            headers: {
+                "Authorization": `Bearer ${authCtx.token}`
+            },
+        })
+        if (!response.ok) {
+            Alert.alert(
+                'Something went wrong!',
+                'Please try again later!'
+            );
+        } else {
+            const data = await response.json();
+            console.log(data)
+            setConnections(data)
+
+        }
+        setIsLoading(false);
+    }, [])
+
+    useEffect(() => {
+        fetchEvent();
+    }, [fetchEvent])
+
+    if (isLoading) {
+        return <LoadingOverlay/>;
+    }
+
     return (
-        <Text>Chat Screen</Text>
+        <View style={styles.container}>
+            {connections.length !== 0 &&
+                <FlatList data={connections} keyExtractor={(connection) => connection.id} renderItem={(itemData) =>
+                    <ChatCard email={itemData.item.email} username={itemData.item.username} />
+                }/>}
+            {connections.length === 0 &&
+                <View style={styles.emptyContainer}>
+                    <Text style={styles.title}>No connections found!</Text>
+                </View>
+            }
+        </View>
     )
 }
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.primary100,
+        borderRadius: 20,
+        padding: 8,
+        margin: 8,
+    },
+    emptyContainer: {
+        backgroundColor: "#FFFFFF",
+        height: 66,
+        width: 350,
+        margin: 4,
+        borderRadius: 8,
+        alignSelf: "center",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20
+    }
+})
--- frontend/screens/ConnectScreen.js
+++ frontend/screens/ConnectScreen.js
@@ -1,45 +1,63 @@
-import React, {useState, useMemo} from 'react'
-import {ImageBackground, Text, View} from 'react-native'
+import React, {useState, useMemo, useCallback, useEffect, useContext} from 'react'
+import {Alert, ImageBackground, Text, View} from 'react-native'
 import TinderCard from 'react-tinder-card'
 import ButtonContained from "../components/ui/ButtonContained";
 import {LinearGradient} from 'expo-linear-gradient';
+import {AuthContext} from "../store/auth-context";
 
-const db = [
-    {
-        name: 'Richard Hendricks',
-        img: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-    },
-    {
-        name: 'Erlich Bachman',
-        img: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-    },
-    {
-        name: 'Monica Hall',
-        img: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-    },
-    {
-        name: 'Jared Dunn',
-        img: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-    },
-    {
-        name: 'Dinesh Chugtai',
-        img: "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px"
-    }
-]
-
-const alreadyRemoved = []
-let charactersState = db // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
+const img = "https://img.bundesliga.com/tachyon/sites/2/2022/11/2223_MD02_SCFBVB_CKB_136-1-scaled.jpg?crop=215px%2C0px%2C2129px%2C1703px";
 
 function ConnectScreen() {
-    const [characters, setCharacters] = useState(db)
-    const [lastDirection, setLastDirection] = useState()
+    const [characters, setCharacters] = useState([])
+    let charactersState = characters;
+    console.log(characters)
 
-    const childRefs = useMemo(() => Array(db.length).fill(0).map(() => React.createRef()), [])
+    const childRefs = useMemo(() => Array(characters.length).fill(0).map(() => React.createRef()), [characters])
 
-    const swiped = (direction, nameToDelete) => {
-        console.log('removing: ' + nameToDelete + ' to the ' + direction)
-        setLastDirection(direction)
-        alreadyRemoved.push(nameToDelete)
+    const authCtx = useContext(AuthContext);
+
+    const fetchUsers = useCallback(async () => {
+        const response = await fetch(`http://localhost:8080/tinder/users`, {
+            headers: {
+                "Authorization": `Bearer ${authCtx.token}`
+            },
+        })
+        if (!response.ok) {
+            Alert.alert(
+                'Something went wrong!',
+                'Please try again later!'
+            );
+        } else {
+            const data = await response.json();
+            console.log(data)
+            setCharacters(data);
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchUsers();
+    }, [fetchUsers])
+
+    async function swipeRequest(id, direction) {
+        const dir = direction === "right" ? "YES" : "NO";
+        console.log(dir)
+        const response = await fetch(`http://localhost:8080/tinder/response/user/${id}`, {
+            method: "POST",
+            body: JSON.stringify({
+                response: dir
+            }),
+            headers: {
+                "Authorization": `Bearer ${authCtx.token}`,
+                "Content-Type": "application/json",
+            },
+        })
+        if (!response.ok) {
+            console.log(response)
+            Alert.alert('Something went wrong!', 'Please try again later!');
+        } else {
+            const data = await response.json();
+            console.log(data)
+        }
     }
 
     const outOfFrame = (name) => {
@@ -49,10 +67,10 @@ function ConnectScreen() {
     }
 
     const swipeButtonHandler = (dir) => {
-        const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.name))
+        const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.username))
         if (cardsLeft.length) {
             const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
-            const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+            const index = characters.map(person => person.username).indexOf(toBeRemoved) // Find the index of which to make the reference to
             alreadyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
             childRefs[index].current.swipe(dir) // Swipe the card!
         }
@@ -62,14 +80,14 @@ function ConnectScreen() {
         <View style={styles.container}>
             <View style={styles.cardContainer}>
                 {characters.map((character, index) =>
-                    <TinderCard ref={childRefs[index]} key={character.name}
-                                onSwipe={(dir) => swiped(dir, character.name)}
-                                onCardLeftScreen={() => outOfFrame(character.name)}>
+                    <TinderCard ref={childRefs[index]} key={index}
+                                onSwipe={async (dir) => await swipeRequest(character.id, dir)}
+                                onCardLeftScreen={() => outOfFrame(character.username)}>
                         <View style={styles.card}>
-                            <ImageBackground style={styles.cardImage} source={{uri: character.img}}>
+                            <ImageBackground style={styles.cardImage} source={{uri: img}}>
                                 <LinearGradient start={{x: 1, y: 0}} end={{x: 0.3, y: 0}}
                                                 colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.8)']} style={styles.cardTitle}>
-                                    <Text style={styles.title}>{character.name}</Text>
+                                    <Text style={styles.title}>{character.username}</Text>
                                 </LinearGradient>
                             </ImageBackground>
                         </View>
